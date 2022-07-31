df42744ed7dee0836edb06ac533271d833f735e0
Harsha Gavli <72166323+hrgavli9@users.noreply.github.com>
2022-08-01 01:31:22 +0530

    Contact us page test done

--- cypress/e2e/HrFolder/ContactUs.cy.js
+++ cypress/e2e/HrFolder/ContactUs.cy.js
@@ -1,20 +1,37 @@
 /// <reference types = "cypress" />
-
 describe('ContactUs Testing', () => {
-    beforeEach(() => {
+    before(() => {
         cy.visit('http://automationpractice.com/index.php')
-        cy.wait(1000)
+    })
+    
+    
+    it('Go to contact us page', () => {
         cy.get('#contact-link > a').click()
+        cy.url().should('eq', 'http://automationpractice.com/index.php?controller=contact')
+        
+        
+        
     })
-
-    //Direct Click on send Button
-    it('Go To contact Page', () => {
+    
+    it('submit contact us', () => {
+        cy.get('#id_contact').select('2')
+        cy.get('#email').type('hrgavali9@gmail.com')
+        cy.get('#id_order').type('order_128978924981')
+        cy.get('#fileUpload').attachFile('../../images/download.jpg')
         cy.get('#submitMessage > span').click()
-        cy.wait(1000)
-        cy.get('ol > li').should('have.text', 'Invalid email address.')
-
+        cy.contains('The message cannot be blank.');
+        
+        cy.get('#message').type('Contrary to popular belief Lorem Ipsum is random text')
+        cy.get('#fileUpload').attachFile('../../images/download.jpg')
+        cy.get('#submitMessage > span').click()
+        cy.contains('Your message has been successfully sent to our team.');
     })
 
-    it
 
+    it('go to home page', () => {
+        cy.get('li > .btn > span').click()
+        cy.url().should('eq', 'http://automationpractice.com/index.php')
+    })
+    
+    
 })
---
+++ cypress/e2e/HrFolder/index.cy.js
@@ -0,0 +1,9 @@
+/// <reference types = "cypress" />
+describe('Automation Example',() => {
+    beforeEach(()=>{
+        cy.visit('http://automationpractice.com/index.php')
+    })
+    
+    
+    
+})
